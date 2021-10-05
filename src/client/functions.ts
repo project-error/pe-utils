6ef0bfbcfa481e8ad34553185fb9b56dc0d52630
Taso <taso@projecterror.dev>
2021-10-05 14:37:22 -0400

    docs: Added generated docs

--- src/client/functions.ts
+++ src/client/functions.ts
@@ -13,3 +13,10 @@ export const RegisterNuiCB = <T = any>(event: string, callback: CallbackFn<T>): 
 };
 
 
+// @ts-ignore
+export const uuidV4 = (options, buf, offset) => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
